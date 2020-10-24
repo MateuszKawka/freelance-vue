43851e03150ea6b0c88a747073f2a8efa915e9a2
Mateusz Kawka <kawkamateusz@pm.me>
2020-10-24 22:09:50 +0200

    add bacis menu & basic boilerplate for project

---
+++ src/components/Menu.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-menu>
+    <ProfilePreview/>
+    <b-menu-list>
+      <b-menu-item tag="router-link" :icon="menuItem.icon" :label="menuItem.label" :to="menuItem.path"
+                   v-for="menuItem in MENU_ITEMS" :key="menuItem.label">
+      </b-menu-item>
+      <b-menu-item icon="information-outline" label="Log out"></b-menu-item>
+    </b-menu-list>
+  </b-menu>
+</template>
+
+<script>
+import ProfilePreview from "@/components/ProfilePreview"
+
+export default {
+  name: "Menu",
+  components: {
+    ProfilePreview
+  },
+  data() {
+    return {
+      MENU_ITEMS
+    }
+  }
+}
+
+
+const MENU_ITEMS = [
+  {
+    label: "Home",
+    path: "/home",
+    icon: "none"
+  },
+  {
+    label: "Calendar",
+    path: "/calendar",
+    icon: "none"
+  },
+  {
+    label: "Notes",
+    path: "/notes",
+    icon: "none"
+  },
+  {
+    label: "Chat",
+    path: "/chat",
+    icon: "none"
+  },
+  {
+    label: "Settings",
+    path: "/settings",
+    icon: "none"
+  }
+]
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+import home from "./modules/home.module";
+import chat from "./modules/chat.module";
+import calendar from "./modules/calendar.module";
+import notes from "./modules/notes.module";
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    modules: {
+        home,
+        chat,
+        calendar,
+        notes
+    }
+});
